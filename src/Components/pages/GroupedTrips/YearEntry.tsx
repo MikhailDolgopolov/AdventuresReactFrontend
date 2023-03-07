2f07376bd84a96ad98f58980358d0c0f5b7069cc
Михаил Долгополов <mishad2304@gmail.com>
2023-03-07 22:15:01 +0300

    TripPoints, issues with forms in modals

--- src/Components/pages/GroupedTrips/YearEntry.tsx
+++ src/Components/pages/GroupedTrips/YearEntry.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import TripBlock from "./TripBlock";
-import {Trip} from "../../../Types"
+import {Trip} from "../../../Helpers/Types"
 
 function YearEntry({props}:{props:Trip[]}) {
 
