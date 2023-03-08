3555850e1e703fccb25adf2678d2e3f9a86d41d3
Михаил Долгополов <mishad2304@gmail.com>
2023-03-08 21:00:06 +0300

    Modal improvements

--- src/Components/pages/TripPage/Trips.tsx
+++ src/Components/pages/TripPage/Trips.tsx
@@ -3,7 +3,7 @@ import TripPage from "./TripPage";
 import {Route, Routes} from "react-router-dom";
 import EmptyRoute from "../EmptyRoute";
 import {get} from "../../../Server/Requests";
-import {City, Country, Person, Trip} from "../../../Types";
+import {City, Country, Person, Trip} from "../../../Helpers/Types";
 
 function Trips({people, cities, countries}:{people:Person[], cities:City[], countries : Country[]}) {
     let [data, setTrips]=useState<Trip[]>([])
@@ -13,13 +13,13 @@ function Trips({people, cities, countries}:{people:Person[], cities:City[], coun
     let routes=
         data.map(trip=>
             <Route path={trip.trip_id.toString()} key={trip.trip_id}
-                   element={<TripPage trip={trip} people={people} cities={cities} countries={countries}/>}>
+                   element={<TripPage trip_id={trip.trip_id} people={people} cities={cities} countries={countries}/>}>
             </Route>
         )
     return (
         <Routes>
             {routes}
-            <Route path="*" element={<EmptyRoute/>}/>
+            <Route path="*" element={<EmptyRoute waiting="trips"/>}/>
         </Routes>
     );
 }
