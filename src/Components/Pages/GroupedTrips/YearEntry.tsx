6d179ba79e6e5f3183bf77d32f781110224efcd6
Михаил Долгополов <mishad2304@gmail.com>
2023-03-29 19:14:39 +0300

    More Modals, SmartWaiter.tsx

---
+++ src/Components/Pages/GroupedTrips/YearEntry.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import useFetch from "../../../Hooks/useFetch";
+import {Trip} from "../../../Helpers/Types";
+import SmartWaiter from "../../../Helpers/SmartWaiter";
+import LoadingError from "../LoadingError";
+import TripBlock from "./TripBlock";
+
+function YearEntry({year, refetch}:{year:number,refetch?:any}) {
+    const [trips, loadTrips, errorTrips] = useFetch<Trip[]>("trips/years/"+year)
+    if(!trips) return <LoadingError loadingObject={"year's trips"} loading={loadTrips}/>
+    const blocks = trips.map(trip=><TripBlock key = {trip.trip_id} trip={trip}/>)
+    return <div className="outline trip-row">
+        <div className="row">
+            <h3>{year} год</h3>
+        </div>
+        <div className="grid">
+            {trips.map(trip=>
+                <TripBlock key = {trip.trip_id} trip={trip}/>)
+            }
+        </div>
+    </div>;
+}
+
+export default YearEntry;
