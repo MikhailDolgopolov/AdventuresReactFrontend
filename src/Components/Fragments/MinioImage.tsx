45eeb4be97619f35633ab90b6562ac7b1e09e806
MikhailDolgopolov <mishad2304@gmail.com>
2024-01-10 19:50:38 +0300

    React Minio Start

---
+++ src/Components/Fragments/MinioImage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+var Minio = require('minio')
+function MinioImage({path}:{path:string}){
+    var minioClient = new Minio.Client({
+        endPoint: 'play.min.io',
+        port: 9000,
+        useSSL: true,
+        accessKey: 'Q3AM3UQ867SPQQA43P2F',
+        secretKey: 'zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG',
+      })
+}
+
+export default MinioImage
